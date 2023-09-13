d039ad15e7992f3ce035d657cc4a857dd8e7163d
mosafari <m.safari2680@gmail.com>
2023-09-13 13:25:25 +0330

    helloworld(imperative)

---
+++ src/io/javabrains/Greeter.java
@@ -0,0 +1,13 @@
+package io.javabrains;
+
+public class Greeter {
+    public void Hello(){
+        System.out.println("Hello World! ");
+    }
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+        greeter.Hello();
+        
+    }
+    
+}
