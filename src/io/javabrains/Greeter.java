c5143605d110f9a28a114cdadcb909a86a03afae
mosafari <m.safari2680@gmail.com>
2023-09-13 15:28:40 +0330

    implements interface using lambda

--- src/io/javabrains/Greeter.java
+++ src/io/javabrains/Greeter.java
@@ -7,9 +7,23 @@ public class Greeter {
     public static void main(String[] args) {
         Greeter greeter = new Greeter();
         HelloWorldGreeter helloWorldGreeter = new HelloWorldGreeter();
-        greeter.Hello(helloWorldGreeter);
+        // greeter.Hello(helloWorldGreeter);
 
+        // implement interface with lambda , to asign lambda to a var
+        // MyLambda myLambdaFunction = (int a)-> System.out.println("Hello World! "); this doesnt work(dosen't have a same signature with interface)
+        // MyLambda myLambdaFunction = ()-> System.out.println("Hello World! ");
+
+        // in our case we use Greeting Interface, so :
+        Greeting myLambdaFunction = ()-> System.out.println("Hello World! ");
+        myLambdaFunction.perform();
         
     }
     
 }
+
+//  interface MyLambda{
+//     // should have same signature with lambdafunc
+//     // and we only should 1 method (to implements it as lambda)
+//     void foo();
+
+// }
