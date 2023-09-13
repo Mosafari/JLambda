effc277ea1f71f9d9d73b17b72cd0f6963bd3c33
mosafari <m.safari2680@gmail.com>
2023-09-13 13:51:51 +0330

    helloworld(oop polymorphism)

--- src/io/javabrains/Greeter.java
+++ src/io/javabrains/Greeter.java
@@ -1,12 +1,14 @@
 package io.javabrains;
 
 public class Greeter {
-    public void Hello(){
-        System.out.println("Hello World! ");
+    public void Hello(Greeting greeting){
+        greeting.perform();
     }
     public static void main(String[] args) {
         Greeter greeter = new Greeter();
-        greeter.Hello();
+        HelloWorldGreeter helloWorldGreeter = new HelloWorldGreeter();
+        greeter.Hello(helloWorldGreeter);
+
         
     }
     
---
+++ src/io/javabrains/HelloWorldGreeter.java
@@ -0,0 +1,10 @@
+package io.javabrains;
+
+public class HelloWorldGreeter implements Greeting {
+    @Override
+    public void perform(){
+        System.out.println("Hello World! ");
+    }
+
+    
+}
