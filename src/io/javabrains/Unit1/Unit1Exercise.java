78789649fe310735aca2e0c321357863411b75f8
mosafari <m.safari2680@gmail.com>
2023-09-13 17:23:35 +0330

    Unit1: Step 2 Solution in Java7/8

--- src/io/javabrains/Unit1/Unit1Exercise.java
+++ src/io/javabrains/Unit1/Unit1Exercise.java
@@ -29,13 +29,37 @@ public class Unit1Exercise {
         // people.forEach(System.out::println);
         //java 8
         Collections.sort(people, (p1, p2) -> p1.getLastName().compareTo(p2.getLastName()));
-        people.forEach(System.out::println);
+        // people.forEach(System.out::println);
         
 		
 		// Step 2: Create a method that prints all elements in the list
-		
+        System.out.println("Printing all persons");
+        // Java 7
+        // printall(people);
+		//Java 8 my solution:
+        people.forEach((p) -> System.out.println(p));
+        // Java 8 tutorial Solution:
+        printConditionally(people, p -> true);
+
 		// Step 3: Create a method that prints all people that have last name beginning with C 
 
 	}
 
+    private static void printConditionally(List<Person> people, Condition condition) {
+        for(Person p: people){
+            if (condition.test(p)){
+                System.out.println(p);
+            }
+        }
+    }
+
+    private static void printall(List<Person> people) {
+        for(Person p : people){
+            System.out.println(p);
+        }
+    }
+
+}
+interface Condition {
+	boolean test(Person p);
 }
